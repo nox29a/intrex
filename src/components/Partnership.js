09b1e1e0995b66bebf66b1306bb37f8fa8daefe2
nox29a <adamczyk29a@interia.pl>
2023-10-26 09:43:36 +0200

    updated partnership

--- src/components/Partnership.js
+++ src/components/Partnership.js
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Partnership = () => {
+  const [selectedId, setSelectedId] = useState(null);
+
+  const handleDivClick = (id) => {
+    setSelectedId(id);
+  };
+
   return (
     <div id="Partnership" className="mx-32 mb-10">
       <div className="text-left">
@@ -13,7 +19,9 @@ const Partnership = () => {
         </div>
         <div className="flex w-full my-4">
           <div className="border rounded w-2/5 p-4 text-sm h-fit">
-            <div className="hidden peer-active/domino:visible">
+            <div
+              className={selectedId === "domino" ? "" : "hidden"}
+            >
               <div>
                 Współpracujemy z firmą Intrex od początku lat 90tych, to bardzo
                 solidny dystrybutor urządzeń do znakowania, ale także producent
@@ -28,7 +36,9 @@ const Partnership = () => {
                 Tim Chapman B.Sc. Business Development Manager (C&E.Europe)
               </div>
             </div>
-            <div className="hidden peer-active/sic:block">
+            <div
+              className={selectedId === "sic" ? "" : "hidden"}
+            >
               <div>
                 asdasdasdasdasdasdnia, ale także producent oraz dostawca
                 komponentów dla Domino. Intrex od zawsze imponował bardzo dużym
@@ -44,48 +54,22 @@ const Partnership = () => {
           </div>
           <div className="w-3/5 mx-10 justify-center flex flex-wrap gap-6 ">
             <div
-              id="domino"
-              active
-              className="peer-active/domino:bg-slate-400 w-1/4 p-3 rounded-xl border h-20 flex justify-center active:border-red-700 items-center hover:scale-110 shadow-xl transform transition duration-500 "
+              onClick={() => handleDivClick("domino")}
+              className={`w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ${
+                selectedId === "domino" ? "border-red-600" : ""
+              }`}
             >
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png" alt="Domino"></img>
             </div>
             <div
-              id="sic"
-              className="peer-active/sic w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 "
+              onClick={() => handleDivClick("sic")}
+              className={`w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ${
+                selectedId === "sic" ? "border-red-600" : ""
+              }`}
             >
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png" alt="SIC"></img>
             </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
-            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
-              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-            </div>
+            {/* Dodaj kolejne divy i obsługę ich kliknięć w podobny sposób */}
           </div>
         </div>
       </div>
