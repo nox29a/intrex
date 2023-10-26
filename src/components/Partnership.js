455f50138a1306ddb25c2a4dd4600229486c351f
nox29a <adamczyk29a@interia.pl>
2023-10-26 09:29:15 +0200

    added prettier

--- src/components/Partnership.js
+++ src/components/Partnership.js
@@ -1,64 +1,96 @@
-import React from 'react';
-
+import React from "react";
 
 const Partnership = () => {
   return (
-<div id='Partnership' className='mx-32 mb-10'>
-<div className='text-left'>
-        <div className='text-gray-400 uppercase'>WSPÓŁPRACA</div>
-        <div className='text-2xl text-black'>Nasi partnerzy <span className='py-2 text-white px-2 rounded-2xl bg-red-600'>o nas</span></div>
-
-        <div className='flex w-full my-4'>
-            <div className='border rounded w-2/5 p-4 text-sm h-fit'>
-                <div>
-                Współpracujemy z firmą Intrex od początku lat 90tych, to bardzo solidny dystrybutor urządzeń do znakowania, ale także producent oraz dostawca komponentów dla Domino. Intrex od zawsze imponował bardzo dużym zaangażowaniem i niezwykle profesjonalnym podejściem w relacjach z klientami. Wsparcie techniczne i serwis szczególnie zasługują na największe uznanie. Mamy nadzieję, że nasza współpraca będzie dalej rozwijana i kontynuowana przez lata.
-                </div>
-                <div className='font-semibold pt-3'>Tim Chapman B.Sc. Business Development Manager (C&E.Europe)</div>
-            </div>
-            <div className='w-3/5 mx-10 justify-center flex flex-wrap gap-6 '>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-                <div className='w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 '>
-                    <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
-                </div>
-            </div>
+    <div id="Partnership" className="mx-32 mb-10">
+      <div className="text-left">
+        <div className="text-gray-400 uppercase">WSPÓŁPRACA</div>
+        <div className="text-2xl text-black">
+          Nasi partnerzy{" "}
+          <span className="py-2 text-white px-2 rounded-2xl bg-red-600">
+            o nas
+          </span>
         </div>
-
+        <div className="flex w-full my-4">
+          <div className="border rounded w-2/5 p-4 text-sm h-fit">
+            <div className="hidden peer-active/domino:visible">
+              <div>
+                Współpracujemy z firmą Intrex od początku lat 90tych, to bardzo
+                solidny dystrybutor urządzeń do znakowania, ale także producent
+                oraz dostawca komponentów dla Domino. Intrex od zawsze imponował
+                bardzo dużym zaangażowaniem i niezwykle profesjonalnym
+                podejściem w relacjach z klientami. Wsparcie techniczne i serwis
+                szczególnie zasługują na największe uznanie. Mamy nadzieję, że
+                nasza współpraca będzie dalej rozwijana i kontynuowana przez
+                lata.
+              </div>
+              <div className="font-semibold pt-3">
+                Tim Chapman B.Sc. Business Development Manager (C&E.Europe)
+              </div>
+            </div>
+            <div className="hidden peer-active/sic:block">
+              <div>
+                asdasdasdasdasdasdnia, ale także producent oraz dostawca
+                komponentów dla Domino. Intrex od zawsze imponował bardzo dużym
+                zaangażowaniem i niezwykle profesjonalnym podejściem w relacjach
+                z klientami. Wsparcie techniczne i serwis szczególnie zasługują
+                na największe uznanie. Mamy nadzieję, że nasza współpraca będzie
+                dalej rozwijana i kontynuowana przez lata.
+              </div>
+              <div className="font-semibold pt-3">
+                asdasdasdasdasdsfasfasfasdasdasdaager (C&E.Europe)
+              </div>
+            </div>
+          </div>
+          <div className="w-3/5 mx-10 justify-center flex flex-wrap gap-6 ">
+            <div
+              id="domino"
+              active
+              className="peer-active/domino:bg-slate-400 w-1/4 p-3 rounded-xl border h-20 flex justify-center active:border-red-700 items-center hover:scale-110 shadow-xl transform transition duration-500 "
+            >
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div
+              id="sic"
+              className="peer-active/sic w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 "
+            >
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+            <div className="w-1/4 p-3 rounded-xl border h-20 flex justify-center items-center hover:scale-110 shadow-xl transform transition duration-500 ">
+              <img src="https://intrex.pl/wp-content/uploads/2023/03/Projekt-bez-tytulu-17.png"></img>
+            </div>
+          </div>
         </div>
-  </div>
-
-  )
+      </div>
+    </div>
+  );
 };
 
 export default Partnership;
